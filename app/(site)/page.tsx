4dde46682ee33b49540e97e8bcaec88221f6b33d
Showkatul islam <showkatul2@gmail.com>
2023-12-14 22:13:26 +0600

    2.4

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,13 @@
+
+import { redirect } from "next/navigation";
+import getSession from "../actions/getSession";
 import AuthForm from "../components/AuthForm";
 import { AiOutlineWechat } from "react-icons/ai";
-export default function Home() {
+export default async function Home() {
+  const session=await getSession()
+  if(session?.user?.email){
+    redirect("/users")
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-center py-12 bg-gray-100">
       <div className="sm:max-w-md sm:mx-auto sm:w-full">
@@ -8,7 +15,7 @@ export default function Home() {
       <AiOutlineWechat className="w-12 h-12 rounded-full text-indigo-600" />
       </div>
         <div className="text-center py-4">
-          <h1 className="font-semibold text-gray-800 text-3xl  tracking-tight ">Sign in your Account</h1>
+          <h1 className="font-semibold text-gray-800 text-3xl  tracking-tight ">Welcome Our Chat Buddy</h1>
         </div>
         <AuthForm/>
       </div>
