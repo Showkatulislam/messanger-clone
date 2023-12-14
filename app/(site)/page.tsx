e0a267572de80df28f8c892ddb59ecb089d6851a
Showkatul islam <showkatul2@gmail.com>
2023-12-14 21:44:59 +0600

    2.2

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,17 +1,10 @@
-import Image from "next/image";
 import AuthForm from "../components/AuthForm";
-
+import { AiOutlineWechat } from "react-icons/ai";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center py-12 bg-gray-100">
       <div className="sm:max-w-md sm:mx-auto sm:w-full">
-        <Image 
-        className="mx-auto w-auto"
-        src="/images/logo.png"
-        alt="logo"
-        width="48"
-        height="48"
-        />
+      <AiOutlineWechat className="w-12 h-12 rounded-full text-indigo-600" />
         <div className="text-center py-4">
           <h1 className="font-semibold text-gray-800 text-3xl  tracking-tight ">Sign in your Account</h1>
         </div>
